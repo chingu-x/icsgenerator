18d7a833b115a7ba7934ac2c8360d3ba253e3bc7
jdmedlock <jdmedlock@gmail.com>
2019-10-30 17:19:46 -0500

    Feature: Create program framework
    Create program framework
    
    Resolves: N/a
    See also: N/a

---
+++ src/Invitation.js
@@ -0,0 +1,23 @@
+const FileOps = require('./FileOps');
+
+class Invitation {
+  /**
+   * Creates an instance of Extract.
+   * @param {String} pathToModel Path to the .ics model file
+   * @param {String} pathToRecipients Path to file containing recipient team details
+   * @param {String} outputFilePath Path to the directory output files will be created in
+   * @memberof Invitation
+   */
+  constructor(pathToModel, pathToRecipients, outputFilePath) {
+    this.pathToModel = pathToModel;
+    this.pathToRecipients = pathToRecipients;
+    this.outputFilePath = outputFilePath;
+  }
+
+  async generate() {
+    console.log('hi Mom');
+  }
+
+}
+
+module.exports = Invitation;
